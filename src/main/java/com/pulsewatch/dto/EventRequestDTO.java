eb7a1cdf3301b5877cf2a552484da45b5fef8347
vishvatejsurve <vishvatejsurve93@gmail.com>
2026-07-24 00:46:47 +0530

    feat: add request validation

--- src/main/java/com/pulsewatch/dto/EventRequestDTO.java
+++ src/main/java/com/pulsewatch/dto/EventRequestDTO.java
@@ -2,6 +2,9 @@ package com.pulsewatch.dto;
 
 import com.pulsewatch.entity.EventStatus;
 import com.pulsewatch.entity.EventType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Data
@@ -9,11 +12,15 @@ import lombok.*;
 @AllArgsConstructor
 public class EventRequestDTO {
 
+    @NotNull
     private EventType eventType;
 
+    @NotBlank
     private String serviceName;
 
+    @NotNull
     private EventStatus status;
 
+    @Positive
     private Integer responseTime;
 }
